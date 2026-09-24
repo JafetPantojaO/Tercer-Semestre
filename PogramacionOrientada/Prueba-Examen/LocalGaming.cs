4052ec72cf56659a8ba80830bd3a2e5569fc0a37
JafetPantojaO <brosa1121@gmail.com>
2026-09-23 21:21:53 -0600

    Cinco borrador

--- PogramacionOrientada/Prueba-Examen/LocalGaming.cs
+++ PogramacionOrientada/Prueba-Examen/LocalGaming.cs
@@ -3,6 +3,8 @@ using System;
 
 public class LocalGaming
 {
+
+    //Numero 4
     public string NombreLocal;
     public List<Equipo> inventario { get; set; }
 
@@ -17,6 +19,15 @@ public class LocalGaming
     public void AgregarEquipo(Equipo nuevo)
     {
         inventario.Add(nuevo);
+
+    }
+
+    public void RentarEquipo(Equipo equipo, Gamer Usuario,  double horas)
+    {
+        TotalTiempo = horas * 10;
+        Usuario.Saldo - TotalTiempo;
+        Console.WriteLine("El usuario {Usuario} rento el {equipo} por {horas} hora/s")
+
 
     }
 }
